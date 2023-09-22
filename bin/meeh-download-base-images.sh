1f792a560d5c0b7927eaf176e9b84f728e20e55a
Mikal Villa <m@sigterm.no>
2023-09-22 15:33:09 +0000

    More scripts

---
+++ bin/meeh-download-base-images.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd /var/lib/libvirt/images
+wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+wget  https://cdimage.debian.org/cdimage/daily-builds/daily/arch-latest/amd64/iso-cd/debian-testing-amd64-netinst.iso
+
